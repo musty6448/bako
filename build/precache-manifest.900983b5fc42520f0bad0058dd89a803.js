fcf618989dab998f1c4defcefa2e590e7938593d
musty6448 <72673816+musty6448@users.noreply.github.com>
2020-10-17 14:32:27 +0100

    Add files via upload

---
+++ build/precache-manifest.900983b5fc42520f0bad0058dd89a803.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2bd4ee59b9a8b8f615a0fb088eeec85",
+    "url": "/bako/index.html"
+  },
+  {
+    "revision": "6fc6e4c3dcab67068250",
+    "url": "/bako/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a612bbde72397250b480",
+    "url": "/bako/static/css/main.81141084.chunk.css"
+  },
+  {
+    "revision": "6fc6e4c3dcab67068250",
+    "url": "/bako/static/js/2.ba1f8f8e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/bako/static/js/2.ba1f8f8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a612bbde72397250b480",
+    "url": "/bako/static/js/main.d6301a69.chunk.js"
+  },
+  {
+    "revision": "c2bf32e9c8d4a9d70e45",
+    "url": "/bako/static/js/runtime-main.f7ced616.js"
+  }
+]);
